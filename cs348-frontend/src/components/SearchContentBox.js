a26ab9459e84aa3bccaaf22097bc21b37b9ec83e
Ethan Guo <ethguo@gmail.com>
2020-07-04 00:36:17 -0400

    fixed a compile error

--- cs348-frontend/src/components/SearchContentBox.js
+++ cs348-frontend/src/components/SearchContentBox.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from "react-redux";
 import { addResults } from '../redux/actions';
 import ChipInput from './ChipInput';
-import TextInput from './TextInput';
 
 const SearchContentBox = ({addResults}) => {
 
