3f9be2201eca235f18dff69584c74a8d3001063d
Robert Nadal <Robnadal44@gmail.com>
2020-07-07 23:29:18 -0400

    Added reactivity to favorites content when modifying favorites

--- cs348-frontend/src/redux/selectors.js
+++ cs348-frontend/src/redux/selectors.js
@@ -1,3 +1,4 @@
 export const getResultsState = store => store.search.results;
 export const getUserState = store => store.user.userId;
+export const getUserFavourites = store => store.user.favourites;
 export const getUserName = store => store.user.userName;
