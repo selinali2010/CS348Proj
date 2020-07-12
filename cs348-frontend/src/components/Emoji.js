14f44c5cac9200f78b828bc471f347bf03125daf
Linda Zheng <thelindazheng@gmail.com>
2020-07-12 02:07:16 -0400

    Make emoji images into components

---
+++ cs348-frontend/src/components/Emoji.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import "./Emoji.css";
+
+import hearteyes from '../assets/hearteyes.png';
+import drooling from '../assets/drooling.png';
+import thumbsup from '../assets/thumbsup.png';
+import thumbsdown from '../assets/thumbsdown.png';
+import throwup from '../assets/throwup.png';
+import skull from '../assets/skull.png';
+
+const emojiMap = Object.freeze({
+  1: hearteyes,
+  2: drooling,
+  3: thumbsup,
+  4: thumbsdown,
+  5: throwup,
+  6: skull,
+})
+
+const Emoji = (props) => {
+  return (
+    <img className="emoji-image" src={emojiMap[props.index]}></img>
+  );
+}
+
+Emoji.propTypes = {
+  index: PropTypes.number
+}
+
+export default Emoji;
